1be7adfb350634ced1bc898c7919886c5723cbc7
shanemitchell <Shane.Mitchell@gmail.com>
2015-03-02 18:41:50 +0000

    commit to allow test push

--- game.js
+++ game.js
@@ -659,8 +659,8 @@ Asteroid = function () {
     if (other.name == "bullet") Game.score += 120 / this.scale;
     this.scale /= 3;
     if (this.scale > 0.5) {
-      // break into fragments
-      for (var i = 0; i < 3; i++) {
+      // break into fragments.
+      for (var i = 0; i < 2; i++) {
         var roid = $.extend(true, {}, this);
         roid.vel.x = Math.random() * 6 - 3;
         roid.vel.y = Math.random() * 6 - 3;
